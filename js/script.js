36a4007562b804a9ba333e4d3a642135b2e733f3
Milos Milijasevic <milosrm20005@gmail.com>
2022-03-13 12:16:17 +0100

    updated

--- js/script.js
+++ js/script.js
@@ -1,1 +1,41 @@
 "use strict";
+
+const slider = function (){
+    const slides = document.querySelectorAll(`.slide`);
+    const buttonLeft = document.querySelector(`.slider-button-left`);
+    const buttonRight = document.querySelector(`.slider-button-right`);
+
+    let currentSlide = 0;
+    const maxSlide = slides.length;
+
+    const goToSlide = (slide) =>{
+        slides.forEach((s, i) => s.style.transform = `translateX(${100 * (i - slide)}%)`);
+    };
+    goToSlide(0);
+
+    const nextSlide = function (){
+        if(currentSlide === maxSlide - 1){
+            currentSlide = 0;
+        }
+        else{
+            currentSlide++;
+        }
+
+        goToSlide(currentSlide);
+    };
+
+    const previousSlide = function (){
+        if(currentSlide === 0){
+            currentSlide = maxSlide - 1;
+        }
+        else{
+            currentSlide--;
+        }
+        goToSlide(currentSlide);
+    };
+
+    buttonLeft.addEventListener(`click`, previousSlide);
+    buttonRight.addEventListener(`click`, nextSlide);
+
+};
+slider();
