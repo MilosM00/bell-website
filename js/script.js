fa871a1d3ff9309c94d609f6edd4ddddfd86293b
Milos Milijasevic <milosrm20005@gmail.com>
2022-03-14 14:37:47 +0100

    updated

--- js/script.js
+++ js/script.js
@@ -38,43 +38,41 @@ const slider = function (){
 
 };
 slider();
+
+
 const teamOverlay = function(){
-    const image = document.querySelector(`.image-team`);
-    const overlay = document.querySelector(`.overlay-team`);
-    
-    image.addEventListener(`mouseenter`, (e) =>{
-        e.preventDefault();
-
-        overlay.style.display = `block`;
-    });
-
-    overlay.addEventListener(`mouseleave`, (e) =>{
-        e.preventDefault();
-        
-        overlay.style.display = `none`;
-    });
-};
-teamOverlay();
-
-const teamText = function (){
-    const words = document.querySelector(`.words`);
+    const image = document.querySelectorAll(`.image-team`);
+    const overlay = document.querySelectorAll(`.overlay-team`);
+    const words = document.querySelectorAll(`.words`);
     const h2 = document.querySelector(`h2`);
     const h4 = document.querySelector(`h4`);
 
-    words.addEventListener(`mouseleave`, (e) =>{
-        e.preventDefault();
+    image.forEach(image => {
+        image.addEventListener(`mouseenter`, (e) =>{
+            e.preventDefault();
 
-        h2.style.color = `black`;
-        h4.style.color = `black`;
-        words.style.backgroundColor = `white`;
+            overlay.forEach(overlay =>{
+                overlay.style.display = `block`;
+            });
+        });
     });
 
-    words.addEventListener(`mouseenter`, (e) =>{
-        e.preventDefault();
+    words.forEach(words =>{
+        words.addEventListener(`mouseenter`, (e) =>{
+            words.style.backgroundColor = `#199eb8`;
+        });
 
-        h2.style.color = `white`;
-        h4.style.color = `white`;
-        words.style.backgroundColor = `#199eb8`;
+        words.addEventListener(`mouseleave`, (e) =>{
+            words.style.backgroundColor = `white`;
+        });
+    });
+
+    overlay.forEach(overlay =>{
+        overlay.addEventListener(`mouseleave`, (e) =>{
+            e.preventDefault();
+
+            overlay.style.display = `none`;
+        });
     });
 };
-teamText();
+teamOverlay();
